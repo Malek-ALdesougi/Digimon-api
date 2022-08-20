0223d44e84b02a9f4bee968684248a6aef6703e2
Malek-ALdesougi <malekaldesougi@gmail.com>
2022-08-20 03:26:26 +0300

    I have added the main files with all main requirements

---
+++ script.js
@@ -0,0 +1,75 @@
+
+
+function Digimon(Name, level, image) {
+
+    this.Name = Name;
+    this.level = level;
+    this.image = image;
+}
+
+
+fetch("https://digimon-api.herokuapp.com/api/digimon").then((result) => {
+
+    console.log(result);
+    let myData = result.json();
+    return myData;
+
+}).then((next) => {
+    next.length = 20;
+    return next;
+
+}).then((finalArray) => {
+
+    console.log(finalArray);
+    loopingArray(finalArray);
+}
+)
+
+function loopingArray(arr) {
+
+    arr.map(newArr => {
+        console.log(newArr.name);
+        console.log(newArr.level);
+        console.log(newArr.img);
+
+
+        console.log(newArr);
+
+        cardMaker(newArr);
+
+    })
+
+    function cardMaker(digimons) {
+
+        let mainDiv = document.getElementById("test");
+
+        let imagesDiv = document.createElement("div");
+        mainDiv.appendChild(imagesDiv);
+        imagesDiv.style.border = "thin solid #FF0000";
+        imagesDiv.style.margin = "5px";
+        imagesDiv.style.backgroundColor = "#FF0000";
+        imagesDiv.style.display = "inline-block";
+
+        let image = document.createElement("img");
+        image.setAttribute("src", digimons.img);
+        // image.style.width = "500";
+        imagesDiv.appendChild(image);
+
+        let p1 = document.createElement("p");
+        p1.textContent = digimons.name;
+        imagesDiv.appendChild(p1);
+
+
+        let p2 = document.createElement("p");
+        p2.textContent = digimons.level;
+        imagesDiv.appendChild(p2);
+
+    }
+}
+
+
+
+
+
+
+
